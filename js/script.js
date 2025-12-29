0d4066d2bdddbc9e17d4d6006e993b54be7d92de
unknown <nao19991999>
2025-12-29 20:37:37 +0900

    リスト追加、js動作追加

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// 歴史セクションの＋と-
 document.querySelectorAll('.history__list__content_ttl_button').forEach(ttl => {
   ttl.addEventListener('click', () => {
     const icon = ttl.querySelector('.toggle_icon');
@@ -11,10 +12,41 @@ document.querySelectorAll('.history__list__content_ttl_button').forEach(ttl => {
   });
 });
 
+
+// ハンバーガーメニュー
 const navBtn = document.getElementById(`js_nav_btn`);
 const navWindow = document.getElementById(`js_nav_window`);
 
 navBtn.addEventListener(`click` ,() =>{
   navBtn.classList.toggle(`is-open`);
     navWindow.classList.toggle(`is-open`);
+});
+
+// モーダル実装
+const modal = document.getElementById(`imgModal`);
+const modalImg = document.getElementById(`imgModalImg`);
+const modalClose = document.getElementById(`imgModalClose`);
+const modalBox = document.querySelector(`.img_modal__box`);
+
+    // クリック対象の画像を全部所得
+const images = document.querySelectorAll(`.content_text02_img img`);
+
+    // 画像クリックでモーダル表示
+images.forEach(img => {
+  img.addEventListener(`click`,() =>{
+    modal.style.display = `flex`;
+    modalImg.src = img.src;
+  } );
+});
+
+    // 背景クリックでも閉じる
+modal.addEventListener(`click` , (e) =>{
+  if(e.target === modal){
+    modal.style.display = `none`;
+  }
+});
+
+    // 閉じるボタン
+modalClose.addEventListener(`click`, () =>{
+  modal.style.display = `none`;
 });
